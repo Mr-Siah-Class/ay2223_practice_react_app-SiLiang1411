553dc6c77ea47ebb22c895c07b8e86e5f66d7401
Tay Si Liang <siliang.21@ichat.sp.edu.sg>
2022-07-12 12:21:03 +0800

    First Commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class HomeScreen extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="Main">
+        <h1>HomeScreen</h1>
+      </div>
+    );
+  }
+}
+
+export default HomeScreen;
